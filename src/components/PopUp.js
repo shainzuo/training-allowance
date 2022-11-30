28ff72681205eab36fb1ab3f3b81c98057849186
ShainZuo <connectsomething@gmail.com>
2022-11-30 22:10:55 +0100

    modified:   src/components/PopUp.js

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -29,7 +29,7 @@ export default function Popup() {
                             </h3>
                             <div className="mt-2">
                             <p className="text-sm text-gray-500">
-                            Thanks for your submission. You will receive your allowance soon. 
+                            Thanks for your submission. You will receive your funding soon. 
                             </p>
                             </div>
                         </div>
